77c1daab606cc5a4c17d5bf53969c85e4ae08a05
Chintan Rita <CRita@walmartlabs.com>
2015-09-14 09:52:19 -0700

    Customizing the selection state of the cell

--- MovieBuff/MovieListCell.swift
+++ MovieBuff/MovieListCell.swift
@@ -13,15 +13,25 @@ class MovieListCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var posterView: UIImageView!
+    var originalColor: UIColor!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        originalColor = titleLabel.textColor
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
-        //super.setSelected(selected, animated: animated)
-
+        super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
+        if (selected) {
+            self.backgroundColor = originalColor
+            titleLabel.textColor = UIColor.blackColor()
+            descriptionLabel.textColor = UIColor.blackColor()
+        } else {
+            titleLabel.textColor = originalColor
+            descriptionLabel.textColor = originalColor
+            self.backgroundColor = UIColor.blackColor()
+        }
     }
 
 }
