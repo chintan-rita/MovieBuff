70e423c036fca6e2b2f81abea4bca7d74d18f847
Chintan Rita <CRita@walmartlabs.com>
2015-09-14 17:42:48 -0700

    Adding animation to the detailed movie view

--- MovieBuff/MovieDetailsViewController.swift
+++ MovieBuff/MovieDetailsViewController.swift
@@ -13,27 +13,83 @@ class MovieDetailsViewController: UIViewController {
 
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var posterView: UIImageView!
+    var animated = true
+    var originalOrigin = CGRect().origin
+    var originalSize = CGRect().size
 
     var movie: NSDictionary = NSDictionary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        originalOrigin = self.descriptionTextView.frame.origin
+        originalSize = self.descriptionTextView.frame.size
+        let aSelector = Selector("animateTextView")
         
+        let _tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: aSelector)
+        
+        self.descriptionTextView.addGestureRecognizer(_tapGesture)
+        
+        // Do any additional setup after loading the view.
+        addTitle()
+        addDescription()
+        loadLowResImage()
+        loadHighResImage()
+    }
+    
+    func animateTextView() {
+        if (animated) {
+            UIView.animateWithDuration(1.0, animations: {
+                self.descriptionTextView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            })
+        }
+        else {
+            UIView.animateWithDuration(1.0, animations: {
+                self.descriptionTextView.frame = CGRect(x: self.originalOrigin.x, y: self.originalOrigin.y, width: self.originalSize.width, height: self.originalSize.height)
+            })
+        }
+        animated = !animated
+    }
+    
+    func addTitle() {
         if let title = movie["title"] as? String {
             self.navigationItem.title = title
         }
-        if let description = movie["synopsis"] as? String {
-            self.descriptionTextView.text = description
+    }
+    
+    func addDescription() {
+        var description:String = ""
+        
+        if let year = movie["year"] as? Int {
+            description += "Year: \(year)\n"
         }
+        
+        if let rating = movie["mpaa_rating"] as? String {
+            description += "Rating: \(rating)\n"
+        }
+        
+        if let reviews = movie["ratings"] as? NSDictionary {
+            if let criticsRating = reviews["critics_rating"] as? NSString {
+                description += "Critics Ratings: \(criticsRating)\n"
+            }
+            
+            if let audienceRating = reviews["audience_rating"] as? NSString {
+                description += "Audience Ratings: \(audienceRating)\n"
+            }
+        }
+        
+        if let synopsis = movie["synopsis"] as? String {
+            description += "\n\(synopsis)"
+        }
+        self.descriptionTextView.text = description
+    }
+    
+    func loadLowResImage() {
         if let images = movie["posters"] as? NSDictionary {
             if let detailedUrl = images["detailed"] as? String {
                 let photoUrl = NSURL(string:detailedUrl)!
                 self.posterView.setImageWithURL(photoUrl)
             }
         }
-        loadHighResImage()
     }
     
     func loadHighResImage() {
